ee44e3b558762ddbdbdd9c8120b9bf24d8f89cd1
Kévin TIASSOU <kevintiassou@gmail.com>
2023-04-06 16:10:06 +0000

    Successful merge

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -5,7 +5,15 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 function Calendar() {
   return (
     <div>
-      <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />
+      <FullCalendar
+        plugins={[dayGridPlugin]}
+        initialView="dayGridMonth"
+        locale="fr"
+        events={[
+          { title: "event 1", date: "2023-04-01" },
+          { title: "event 2", date: "2023-04-02" },
+        ]}
+      />
     </div>
   );
 }
