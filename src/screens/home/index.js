ed25fddcd829c2aab9dd06adfc13152a3efaf989
Dell <eyramativon1@gmail.com>
2023-04-06 17:23:48 +0200

    Login & register

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -16,13 +16,11 @@ import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import HistoryEduIcon from "@mui/icons-material/HistoryEdu";
 import AssuredWorkloadIcon from "@mui/icons-material/AssuredWorkload";
 import ClassIcon from "@mui/icons-material/Class";
-import Calendar from "./Calendar";
+import Calendar from "../../components/Calendar"
 
 const drawerWidth = 240;
 
