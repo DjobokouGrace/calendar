a7d7bf9ab161fd409d7646b6405c8d4debb12046
Kévin TIASSOU <kevintiassou@gmail.com>
2023-04-07 14:38:49 +0000

    Trying to implement events

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import listPlugin from "@fullcalendar/list";
 import timeGridPlugin from "@fullcalendar/timegrid";
+import axios from "axios";
 
 function Calendar() {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    axios.get("/event.json").then((res) => setEvents(res.data));
+  }, []);
+
   return (
     <div>
       <FullCalendar
